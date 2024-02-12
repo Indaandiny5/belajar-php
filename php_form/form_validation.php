c904a9500fd1fd475b3ac6cfd49075784aafeae7
indaandiny5 <2223598.inda@smkn-2sbg.sch.id>
2024-02-12 09:35:50 +0700

    php

---
+++ php_form/form_validation.php
@@ -0,0 +1,127 @@
+<!DOCTYPE HTML>  
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE-edge">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Document</title>
+</head>
+<style>
+th{
+    background-color: rgb(82, 124, 165) ;
+}
+</style>
+<body>
+<h1 align="center">Biodata Diri</h1>
+
+<table width="800px" height="200" border="1px" align="center" celpadding="5">
+    <tr>
+        <th> Photo </th>
+        <th colspan="2">Identitas Diri</th>
+    
+    </tr>
+
+    <tr>
+        <td rowspan="7">
+            <img src="inda6.jpg" width="200" height="200">
+
+        </td>
+        <td>
+            Nama Lengkap
+        </td>
+        <td> INDA ANDINY</td>
+    </tr>
+
+    <tr>
+        <td>
+            Tempat Tanggal Lahir
+        </td>
+        <td>Subang, 05 Desember 2006</td>
+    </tr>
+
+    <tr>
+        <td>
+            Alamat
+        </td>
+        <td>Jl.Raya Blok Tugu Utara, Rt/14 Rw/05, Desa Kalijati Barat, Kecamatan Kalijati</td>
+    </tr>
+
+    <tr>
+        <td>
+            Agama
+        </td>
+        <td>Islam</td>
+    </tr>
+
+    <tr>
+        <td>
+            Hobi
+        </td>
+        <td>Membaca</td>
+    </tr>
+
+    <tr>
+        <td>
+            Jenis Kelamin
+        </td>
+        <td>Perempuan</td>
+    </tr>
+</table>
+</head>
+<body>  
+
+<center>
+<?php
+// define variables and set to empty values
+$name = $email = $gender = $comment = $website = "";
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  $name = test_input($_POST["name"]);
+  $email = test_input($_POST["email"]);
+  $website = test_input($_POST["website"]);
+  $comment = test_input($_POST["comment"]);
+  $gender = test_input($_POST["gender"]);
+}
+
+function test_input($data) {
+  $data = trim($data);
+  $data = stripslashes($data);
+  $data = htmlspecialchars($data);
+  return $data;
+}
+?>
+
+<h2>PHP Form Validation Example</h2>
+<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">  
+  Name: <input type="text" name="name">
+  <br><br>
+  E-mail: <input type="text" name="email">
+  <br><br>
+  Website: <input type="text" name="website">
+  <br><br>
+  Comment: <textarea name="comment" rows="5" cols="40"></textarea>
+  <br><br>
+  Gender:
+  <input type="radio" name="gender" value="female">Female
+  <input type="radio" name="gender" value="male">Male
+  <input type="radio" name="gender" value="other">Other
+  <br><br>
+  <input type="submit" name="submit" value="Submit">  
+</form>
+
+<?php
+echo "<h2>Your Input:</h2>";
+echo $name;
+echo "<br>";
+echo $email;
+echo "<br>";
+echo $website;
+echo "<br>";
+echo $comment;
+echo "<br>";
+echo $gender;
+?>
+</center>
+
+</body>
+</html>
